48f57142e7d5f45f998de9358400aeee42200d8f
Shannon Lin <shannon.n.lin@gmail.com>
2023-08-11 12:44:54 -0700

    Remove duplicate code

--- config/passport.js
+++ config/passport.js
@@ -4,53 +4,6 @@ const User = require('../models/User')
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
-        if (err) {
-          return done(err)
-        }
-        if (!user) {
-          return done(null, false, { msg: `Email ${email} not found`})
-        }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err)
-          }
-          if (isMatch) {
-            return done(null, user)
-          }
-          return done(null, false, { msg: 'Invalid email or password'})
-        })
-      })
-    })
-  )
-
-  passport.use(
-    new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
-      async function findUser() {
-        try {
-          let data = await User.findOne({ email: email } )
-          if (!data) {
-            console.log(`Email ${email} not found`)
-            return done(null, false, { msg: `Email ${email} not found` })
-          }
-          user.comparePassword(password, (err, isMatch) => {
-            if (err) {
-              return done(err)
-            }
-            if (isMatch) {
-              return done(null, user)
-            }
-            return done(null, false, { msg: 'Invalid email or password'})
-          })
-        } catch (err) {
-          console.log(err)
-        }
-      }
-    })
-  )
-
-  passport.use(
      new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
       User.findOne({ email: email }).then(user => {
         if (!user) {
@@ -70,26 +23,15 @@ module.exports = function (passport) {
      })
   )
 
-
-
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
   
-  // passport.deserializeUser((id, done) => {
-  //   // User.findById(id, (err, user) => done(err, user))
-  //   User.findById(id).then((err, user) => {
-
-  //       done(err, user)
-
-  //   })
-  // })
-  passport.deserializeUser((id, done)=>{
-
+  passport.deserializeUser((id, done) => {
     User.findById(id).then((user) => {
-      done(null, user);
-    }).catch(done);
 
-});
+        done(null, user)
 
+    }).catch(done)
+  })
 }
