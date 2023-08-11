9b4797b4f89288cb91d31ea46a5464549455636e
Shannon Lin <shannon.n.lin@gmail.com>
2023-08-11 12:35:30 -0700

    Fix login

--- config/passport.js
+++ config/passport.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  // passport.use(
-  //   new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
-  //     User.findOne({ email: email.toLowerCase() }, (err, user) => {
-  //       if (err) {
-  //         return done(err)
-  //       }
-  //       if (!user) {
-  //         return done(null, false, { msg: `Email ${email} not found`})
-  //       }
-  //       user.comparePassword(password, (err, isMatch) => {
-  //         if (err) {
-  //           return done(err)
-  //         }
-  //         if (isMatch) {
-  //           return done(null, user)
-  //         }
-  //         return done(null, false, { msg: 'Invalid email or password'})
-  //       })
-  //     })
-  //   })
-  // )
+  passport.use(
+    new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+      User.findOne({ email: email.toLowerCase() }, (err, user) => {
+        if (err) {
+          return done(err)
+        }
+        if (!user) {
+          return done(null, false, { msg: `Email ${email} not found`})
+        }
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) {
+            return done(err)
+          }
+          if (isMatch) {
+            return done(null, user)
+          }
+          return done(null, false, { msg: 'Invalid email or password'})
+        })
+      })
+    })
+  )
 
   passport.use(
     new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
@@ -50,11 +50,46 @@ module.exports = function (passport) {
     })
   )
 
+  passport.use(
+     new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+      User.findOne({ email: email }).then(user => {
+        if (!user) {
+          console.log(`Email ${email} not found`)
+          return done(null, false, { msg: `Email ${email} not found` })
+        }
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) {
+            return done(err)
+          }
+          if (isMatch) {
+            return done(null, user)
+          }
+          return done(null, false, { msg: 'Invalid email or password'})
+        })
+      })
+     })
+  )
+
+
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
   
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
+  // passport.deserializeUser((id, done) => {
+  //   // User.findById(id, (err, user) => done(err, user))
+  //   User.findById(id).then((err, user) => {
+
+  //       done(err, user)
+
+  //   })
+  // })
+  passport.deserializeUser((id, done)=>{
+
+    User.findById(id).then((user) => {
+      done(null, user);
+    }).catch(done);
+
+});
+
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -105,11 +105,15 @@ const postLogin = (req, res, next) => {
 }
 
 const logout = (req, res) => {
-  req.logout(function(err) { 
-    console.log('User has logged out')
-    if (err) {return next(err)}
-    res.redirect('/') 
+  req.logout(() => {
+    console.log('User has logged out.')
   })
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
 }
 
 module.exports = {
