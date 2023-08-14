6690c6bb4bbaf1cc79ccaacaa1c15fabf3443a6f
Shannon Lin <shannon.n.lin@gmail.com>
2023-08-14 16:19:02 -0700

    Downgrade Passport because of req.session.regenerate issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -105,15 +105,14 @@ const postLogin = (req, res, next) => {
 }
 
 const logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
+  req.logout()
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
     req.user = null;
     res.redirect("/");
   });
+  console.log('User has logged out.')
 }
 
 module.exports = {
